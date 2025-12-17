7407421bbb92b47be5db9de1fbb917e8a9d1e0e2
Mario Taracena <mario.taracena@herodigital.com>
2025-12-17 11:41:26 -0600

    adding base functionality and base styles for dynamic cards

--- blocks/dynamic-cards/dynamic-cards.js
+++ blocks/dynamic-cards/dynamic-cards.js
@@ -1,3 +1,51 @@
+import {
+  createOptimizedPicture,
+} from '../../scripts/aem.js';
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json();
+}
+
 export default function decorate(block) {
-  console.log('block', block);
+  console.log('dynamic-cards', block);
+  const source = block.querySelector('a[href]') ? block.querySelector('a[href]').href : '/query-index.json';
+  block.innerHTML = '';
+  const ul = document.createElement('ul');
+
+  fetchData(source).then((data) => {
+    console.log('data', data);
+
+    data.data.forEach((item) => {
+      const li = document.createElement('li');
+      const imageDiv = document.createElement('div');
+      imageDiv.setAttribute('class', 'cards-card-image');
+
+      const pic = createOptimizedPicture(item.image, '', false, [{ width: '375' }]);
+      imageDiv.appendChild(pic);
+
+      const bodyDiv = document.createElement('div');
+      bodyDiv.setAttribute('class', 'cards-card-body');
+
+      const p = document.createElement('p');
+      const strong = document.createElement('strong');
+      strong.textContent = item.title;
+      p.appendChild(strong);
+
+      const p2 = document.createElement('p');
+      p2.textContent = item.description;
+
+      bodyDiv.appendChild(p);
+      bodyDiv.appendChild(p2);
+
+      li.appendChild(imageDiv);
+      li.appendChild(bodyDiv);
+      ul.appendChild(li);
+    });
+
+    block.appendChild(ul);
+  });
 }
